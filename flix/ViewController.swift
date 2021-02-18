a4178b9b150401abbcd436d123bd6af19ebc15ce
Zain Makda <zain.makda@stonybrook.edu>
2021-02-17 20:51:00 -0500

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by sully on 2/17/21.
+//  Copyright © 2021 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
